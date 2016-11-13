9d4ec2bde1b593a50da2eff5dbc74701839b23d3
Anatoly Smolyaninov <zarkone@ya.ru>
2016-11-13 09:47:39 +0700

    Reagent init; show snow level for Barnaul

--- src/cljs/itrains/app.cljs
+++ src/cljs/itrains/app.cljs
@@ -1,6 +1,48 @@
-(ns itrains.app)
+(ns itrains.app
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require
+   [reagent.core :as r]
+   [cljs.core.async :refer [<!]]
+   [itrains.weather :as weather]))
+
+(defn console-log [& args]
+  (apply (.-log js/console) args))
+
+(defn console-dir [cljs-object]
+  (.dir js/console (clj->js cljs-object)))
+
+(def current-city (r/atom "Barnaul"))
+(def current-weather (r/atom {}))
+
+
+(let [w (weather/weather-channel @current-city :count 1)]
+  (console-log "fetch weatcher...")
+  (go
+    (swap! current-weather assoc @current-city (<! w))))
+
+;; (console-dir @current-weather)
+
+(get @current-weather @current-city)
+(defn get-snow [weather-map]
+  (-> weather-map
+    :body
+    :list
+    (get 0)
+    :snow))
+
+(defn weather-component [current-city current-weather]
+  (when-let [w (get @current-weather @current-city)]
+    (let [snow (get-snow w)]
+      (console-dir w)
+      [:div
+       [:h2 @current-city]
+       [:h3 "Снег: " snow]])))
 
 (defn init []
-  (let [c (.. js/document (createElement "DIV"))]
-    (aset c "innerHTML" "<p>i'm dynamically created</p>")
-    (.. js/document (getElementById "container") (appendChild c))))
+  [weather-component current-city current-weather])
+
+(defn ^:export run []
+  (r/render [init]
+            (js/document.getElementById "app")))
+
+(run)
